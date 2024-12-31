e4d5796ae36e30fb585d8b689eede94fcba5b57f
prabath1998 <prabhathudayanga1998@gmail.com>
2025-01-01 05:29:10 +0530

    initialize clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    // <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
