528abaee4703002de817879ee19b3eb7c55c8dcc
prabath1998 <prabhathudayanga1998@gmail.com>
2025-01-01 20:47:47 +0530

    add shadcn and create base layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+
+export function ButtonDemo() {
+  return <Button>Button</Button>
+}
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="mt-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+          
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <ModeToggle/>
     </div>
   );
 }
