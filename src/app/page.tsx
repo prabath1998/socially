62c20e4d45ecafb853fd176a7d33050c65d17260
prabath1998 <prabhathudayanga1998@gmail.com>
2025-01-01 21:56:32 +0530

    add navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,14 @@ import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-
 export function ButtonDemo() {
-  return <Button>Button</Button>
+  return <Button>Button</Button>;
 }
 
 export default function Home() {
   return (
     <div className="mt-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-          
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle/>
+     <h1>Home </h1>
     </div>
   );
 }
